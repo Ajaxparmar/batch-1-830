dfc464165c9112c20710df3baf85694d0d733009
Ajaxparmar <ajaxparmar@gmail.com>
2026-07-07 09:40:41 +0530

    added new code base

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+
+
+// {
+// let a=90 
+// a='deepak'
+// // var username='vijay'
+// const user='ravi'
+// username='mohit'
+// console.log(typeof(a));
+
+// let mystring=`Hello ${username} welcome to code scaler`
+// // console.log("Hello class",a,username,user);
+// console.log(mystring);
+
+// }
+
+// // let a=90;
+
+// // const mulist=[3,4,5]
+// // mulist[2]=4
+// // console.log(mulist);
+
+
+// // let value= a+b
+// // let mul=a*b
+// // let values= a+(a/2)+34/2
+// let a=2
+// let c="4"
+// let b=3
+// let p=a+b+c
+// console.log(typeof(p), p);
+
+
+let obj={name: 'vijay',
+     rollno: 123, 
+     class: 'MCA',
+     subjects: ['maths', 'science', 'english'],
+     disabled: false,
+     myFun: function sayHello() {
+        console.log('Hello');
+     },
+     edu:{ 
+        college: 'ABC College',
+        year: 2021
+     }
+
+}
+
+
+
+
---
+++ index1.js
@@ -0,0 +1,35 @@
+// let a=90
+// a=39
+// console.log(a);
+// const b=90
+// const v=[3,3,4]
+// v[2]=90
+
+// console.log(v)
+
+
+let a=990
+console.log(typeof(a));
+let b='sumit'
+console.log(typeof(b));
+let c=true
+console.log(typeof(c));
+let d
+console.log(typeof(d));
+let e=null
+console.log(typeof(e));
+let f={username:"vijat"}
+console.log(typeof(f));
+let g= [3,4,5]
+console.log(typeof(g));
+sum=4+4+3+(d)
+console.log(sum);
+
+
+
+
+
+
+
+
+
